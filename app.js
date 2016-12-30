3df70f01fc780dbd4068eb2892a228e638534226
Ubuntu <ubuntu@ip-172-31-27-182.us-west-2.compute.internal>
2016-12-30 04:38:53 +0000

    Initializes server 'app.js' and finishes setup with grunt watch, node-dev, and screen.

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
-//app.js
+var express = require("express");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+var app = express();
+
+var skierTerms = [
+    {
+	term: "rip",
+	defined: "to move at a high rate of speed",
+    },
+    {
+	term: "Huck",
+	defined: "to throw your body off of something, usually a natural feature like a cliff"
+    },
+    {
+	term: "chowder",
+	defined: "powder after it has been sufficiently skied"
+    }
+];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function(req, res, next) {
+    console.log(`${req.method} request for '${req.url}' - ${JSON.stringify(req.body)}`);
+    next();
+});
+
+app.use(express.static("./public"));
+
+app.use(cors());
+
+app.get("/dictionary-api", function(req, res) {
+    res.json(skierTerms);
+});
+
+app.post("/dictionary-api", function(req, res) {
+    skierTerms.push(req.body);
+    res.json(skierTerms);
+});
+
+app.delete("/dictionary-api/:term", function(req, res) {
+    skierTerms = skierTerms.filter(function(definition) {
+	return definition.term.toLowerCase() !== req.params.term.toLowerCase();
+    });
+    res.json(skierTerms);
+});
+
+app.listen(8080);
+
+console.log("Express app running on port 8080");
+
+module.exports = app;
