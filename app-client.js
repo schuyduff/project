97aaa4e454064bdd6896775e7a01f7a43ad63758
Ubuntu <ubuntu@ip-172-31-27-182.us-west-2.compute.internal>
2017-06-01 19:48:18 +0000

    refactored scatterplot 3

--- app-client.js
+++ app-client.js
@@ -2,21 +2,39 @@ var $ = require("jquery");
 var d3 = require("d3");
 var scatterplot = require("./lib/scatterplot.js");
 var scatterplot2 = require("./lib/scatterplot2.js");
+var scatterplot3 = require("./lib/scatterplot3.js");
 var io = require('socket.io');
+var form = require("./lib/form.js");
 
 $(document).ready(function(){
+    form.date();
 
-    scatterplot2.daily_PPFD("20150103");
-   // scatterplot.annual_DLI("./assets/2015.json");
+    
+	scatterplot3.PPFD_daily_new("20150103");
+
+    
+    /*
     $('#_date_').submit(function(event){
+
+	var year = $('input[name=year]:checked').val();
+	var month =('0'+$('input[name=month]:checked').val()).slice(-2);
+	var day =  ('0'+$('input[name=day]:checked').val()).slice(-2);
+	var input = ""+year+month+day;
+
+	scatterplot3.JSONfilenames(input, function(filenames){
+	    scatterplot3.PPFD_daily_new(input,filenames);
+
+	});
 	
-
-
-	var input = $("input").val().replace(/-/gi,"");
-	console.log(input);
-	scatterplot2.daily_PPFD(input);
+	
+	
 	event.preventDefault();
+
+	
     });
+    */
+    
+
 /*
     document.forms[0].onsubmit= function(){
 	var input = document.getElementById('_date_').value;
