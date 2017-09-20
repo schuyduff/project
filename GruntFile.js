ab3698c70cbf69d1941005b5bb393bd3fce0bbcf
Ubuntu <ubuntu@ip-172-31-27-182.us-west-2.compute.internal>
2017-09-20 19:56:08 +0000

    installed bootstrap

--- GruntFile.js
+++ GruntFile.js
@@ -10,9 +10,18 @@ module.exports = function(grunt){
 	    }
 	},
 	less:{
-	    production:{
+	    
+	    development:{
+		options:{
+		    compress: true,
+		    yuicompress: true,
+		    optimization: 2
+		},
 		files:{
-		    "public/css/style.css":["less/*.less"]
+		    "public/css/style.css":[
+			"less/*.less"
+		    ],
+		  
 		}
 	    }
 
@@ -31,23 +40,38 @@ module.exports = function(grunt){
 	},
 	watch:{
 	    css:{
-		files:["less/*.less"],
+		files:["less/*.less","sass/*.scss"],
 		tasks:["css"]
 	    },
 	    scripts:{
 		files:["app-client.js","lib/*.js"],
 		tasks:["jshint","browserify"]
 	    }
+	},
+	cssmin:{
+	    compress:{
+		files:{
+		    'public/css/style.min.css':[
+			'public/css/style.css'
+			
+		    ]
+		}
+	    }
+
 	}
+
     });
+    grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks("grunt-browserify");
     grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks("grunt-autoprefixer");
     grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks("grunt-contrib-watch");
-
-    grunt.registerTask("css",["less","autoprefixer"]);
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    
+    grunt.registerTask("css",["less","autoprefixer","cssmin"]);
     grunt.registerTask("js",["browserify"]);
 
 
